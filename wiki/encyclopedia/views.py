47593fc80c24aaa93ac98a094bcdab8a8a8c28ef
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-08-23 00:45:05 +0100

    Part 2 of user story 1 - added map fuction with logic to display error message if URL TITLE not within list of wikis.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
 
 def index(request):
+    print(util.list_entries())
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def wiki(request, title):
-    return render(request, "encyclopedia/wiki.html", {
-        "title": title
-    })
+    if title.lower() in map(lambda x:x.lower(), util.list_entries()):
+       return render(request, "encyclopedia/wiki.html", {
+           "title": title
+        })
+    else:
+        return HttpResponse("<h1>Page not yet created</h1>") 
