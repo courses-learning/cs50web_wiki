9f624b4986ed5aa46b06c5cfbc9816a568ca0e71
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-08-22 23:41:50 +0100

    First commit - working URL route for first user story to a /TITLE page. Still needs template building and error page.

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def wiki(request, title):
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title
+    })
