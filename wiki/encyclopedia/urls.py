71833d37d40880e44c2a617c1ad44bba9ae5d34a
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-09-06 12:22:12 +0100

    New page user story complete for functionality. Form and message for entry already existing need work to tidy UI.

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
     path("search", views.search, name="search"),
+    path("new", views.new, name="new"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+from django import forms
 
 from . import util
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="New Title")
+    markup = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":20}), label="")
 
 
 def index(request):
@@ -9,36 +17,61 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def wiki(request, title):
     if title.lower() in map(lambda x:x.lower(), util.list_entries()):
-        print(util.get_entry(title))
         return render(request, "encyclopedia/wiki.html", {
-        "title": title,
-        "entry": util.get_entry(title)
+            "title": title,
+            "entry": util.get_entry(title)
         })
     else:
         return HttpResponse("<h1>Page not yet created</h1>") 
 
+
 def search(request):
         all_titles = util.list_entries()
         partial_matches = []
+
         for title in all_titles:
+            # Searched title already exists -> goto page
             if title.lower() == request.GET["q"].lower():
-                # potential for optimisation here...
-                return render(request, "encyclopedia/wiki.html", {
-                    "title": title,
-                    "entry": util.get_entry(title)
-                    })
+                return redirect('wiki', title)
+            # Partial match found against current iterable -> add to partial_match list
             elif request.GET["q"].lower() in title.lower():
                 partial_matches.append(title)
+            # No match current iterable
             else:
                 continue
-        if len(partial_matches) > 0:
-            return render(request, "encyclopedia/search.html", {
-                "partial_matches": partial_matches
-            })
-        else:
-            # No matches at all
-            print("No Match")
         
-        return render(request, "encyclopedia/search.html")
+        return render(request, "encyclopedia/search.html", {
+            "partial_matches": partial_matches
+        })
+
+
+def new(request):
+
+    # POST so submission of new title/entry has occurred
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            markup = form.cleaned_data["markup"]
+
+            # new title already exists
+            if title.lower() in map(lambda x:x.lower(), util.list_entries()):
+                messages.info(request, 'Already an entry for this')
+                return HttpResponseRedirect(reverse("index"))
+            # Create new title/entry, save and goto that new page
+            else:
+                util.save_entry(title, markup)
+                return redirect('wiki', title)
+
+        # Catch if form POST with invalid data 
+        else:
+            return HttpResponse("<h1>Something went wrong - Invalid data submitted</h1>") 
+
+    # Initial load via GET so render blank form
+    else:
+        return render(request, "encyclopedia/new.html", {
+            "form": NewEntryForm()
+        })
